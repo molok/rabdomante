a1686c03d77337d2973551f26feda8a2e7ce97d0
Alessio Bolognino <alessio.bolognino@gmail.com>
2017-12-19 12:05:43 +0100

    Revert "revert dei danni fatti per debuggare il codice"
    This reverts commit 7c96bf6b54fb961f427392bc1d65b64aac4ff92d.

--- src/main/java/alebolo/rabdomante/SolverTarget.java
+++ src/main/java/alebolo/rabdomante/SolverTarget.java
@@ -13,7 +13,10 @@ public class SolverTarget implements IMgPerL {
     public Water water() { return water; }
     public int calcioMgPerL() { return toInt(water.recipe().calcioMgPerL()); }
     public int magnesioMgPerL() { return toInt(water.recipe().magnesioMgPerL()); }
-    public int sodioMgPerL() { return toInt(water.recipe().sodioMgPerL()); }
+    public int sodioMgPerL() {
+        System.out.println("sodioperMgLDouble:" + water.recipe().sodioMgPerL());
+        return toInt(water.recipe().sodioMgPerL());
+    }
     public int bicarbonatiMgPerL() { return toInt(water.recipe().bicarbonatiMgPerL()); }
     public int cloruroMgPerL() { return toInt(water.recipe().cloruroMgPerL()); }
     public int solfatoMgPerL() { return toInt(water.recipe().solfatoMgPerL()); }
