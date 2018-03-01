90255833a0a368e2c3f39ae4015efb87cab06949
Alessio Bolognino <alessio.bolognino@gmail.com>
2018-03-01 17:54:43 +0100

    cose

--- src/test/java/alebolo/rabdomante/CS3.java
+++ src/test/java/alebolo/rabdomante/CS3.java
@@ -1,7 +1,8 @@
 package alebolo.rabdomante;
 
-import com.google.common.collect.Lists;
-import org.apache.commons.collections4.ListUtils;
+import com.google.common.primitives.Ints;
+import org.apache.commons.collections4.IteratorUtils;
+import org.apache.commons.math3.util.IntegerSequence;
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.variables.IntVar;
@@ -34,26 +35,20 @@ public class CS3 {
     @Test public void x() {
         Water distillata = new Water(0, 0, 0);
         Water sa = new Water(50, 20, 20);
-        Water sindaco = new Water(100, 100, 100);
+        Water milano = new Water(100, 100, 100);
         Water target = new Water(75, 75, 75);
-//        Water boh = new Water(15, 15, 15);
-        int targetL = 100;
+        int targetL = 100; /* a 1000 si spacca... qualche overflow ? */
 
         Model model = new Model("waterModel");
 
-        double litersPrecision = 0.1;
-
-//        public final static MineralProfile GYPSUM = new MineralProfile.Builder().name("gypsum").calcioRatio(0.2328).solfatoRatio(0.558).build();
-//        public final static MineralProfile TABLE_SALT = new MineralProfile.Builder().name("tableSalt").sodioRatio(0.3934).cloruroRatio(0.6066).build();
-
         Salt gypsum = new Salt(23, 0, 0);
         Salt tableSalt = new Salt(0, 0, 39);
-        IntVar vGypsum = model.intVar("gypsum", 0, 100);
-        IntVar vTableSalt = model.intVar("tablesalt", 0, 100);
+        IntVar vGypsum = model.intVar("gypsum (g)", 0, 100);
+        IntVar vTableSalt = model.intVar("tablesalt (g)", 0, 100);
 
-        IntVar vDistillata = model.intVar("distillata", 0, targetL);
-        IntVar vSantanna = model.intVar("santanna", 0, targetL);
-        IntVar vSindaco = model.intVar("sindaco", 0, targetL);
+        IntVar vDistillata = model.intVar("distillata (L)", range(targetL, 10));
+        IntVar vSantanna = model.intVar("santanna (L)", range(targetL, 10));
+        IntVar vMilano = model.intVar("milano (L)", range(targetL, 10));
 
         Map<Salt, IntVar> ss = new HashMap<>();
         ss.put(gypsum, vGypsum);
@@ -62,19 +57,11 @@ public class CS3 {
         Map<Water, IntVar> wvs = new HashMap<>();
         wvs.put(distillata, vDistillata);
         wvs.put(sa, vSantanna);
-        wvs.put(sindaco, vSindaco);
+        wvs.put(milano, vMilano);
 
-        IntVar sumCaSalt = sumSalt(ss, s -> s.ca);
-        IntVar sumMgSalt = sumSalt(ss, s -> s.mg);
-        IntVar sumNaSalt = sumSalt(ss, s -> s.na);
-
-        IntVar sumCa = sum(wvs, w1 -> w1.ca);
-        IntVar sumMg = sum(wvs, w1 -> w1.mg);
-        IntVar sumNa = sum(wvs, w1 -> w1.na);
-
-        IntVar sumCaWs = sumCa.add(sumCaSalt).intVar();
-        IntVar sumMgWs = sumMg.add(sumMgSalt).intVar();
-        IntVar sumNaWs = sumNa.add(sumNaSalt).intVar();
+        IntVar sumCaWs = waterSum(wvs, w1 -> w1.ca).add(sumSalt(ss, s -> s.ca)).intVar();
+        IntVar sumMgWs = waterSum(wvs, w1 -> w1.mg).add(sumSalt(ss, s -> s.mg)).intVar();
+        IntVar sumNaWs = waterSum(wvs, w1 -> w1.na).add(sumSalt(ss, s -> s.na)).intVar();
 
         IntVar cost = square(sumCaWs.sub(target.ca * targetL).intVar())
                  .add(square(sumMgWs.sub(target.mg * targetL).intVar()))
@@ -95,11 +82,27 @@ public class CS3 {
             toWatch.stream().forEach(w -> System.out.println(":"+w.toString()));
             System.out.println("target:" + target.toString());
             System.out.printf(
-                    "ca: " + weightedSum(wvs, w -> w.ca, targetL) +
-                            ", mg: " + weightedSum(wvs, w -> w.mg, targetL) +
-                            ", na: " + weightedSum(wvs, w -> w.na, targetL) + "\n");
+                    "Ca (mg/L): " + weightedSum(wvs, w -> w.ca, ss, s -> s.ca, targetL) +
+                            ", Mg (mg/L): " + weightedSum(wvs, w -> w.mg, ss, s -> s.mg, targetL) +
+                            ", Na (mg/L): " + weightedSum(wvs, w -> w.na, ss, s -> s.na, targetL) + "\n");
         }
 
+    }
+
+    /* ritorna un range con massimo targetL e step ogni perc, ad esempio:
+        range(100, 10)  -> [0, 10, 20, 30, 40, 50, 60, 70, 80 , 90, 100]
+        range(100, 50)  -> [0, 50, 100]
+        range(100, 100) -> [0, 100]
+        range(10, 10)   -> [1, 2, 3, 4 5, 6, 7, 8, 9, 10]
+     */
+    private int[] range(int targetL, int perc) {
+        if ((targetL / perc ) <= 0 ) {
+            throw new RuntimeException("targetL / perc <= 0");
+        } else {
+            return Ints.toArray(
+                    IteratorUtils.toList(
+                            new IntegerSequence.Range(0, targetL, targetL / perc).iterator()));
+        }
     }
 
     private IntVar sumSalt(Map<Salt, IntVar> ss, Function<Salt, Integer> f) {
@@ -107,21 +110,17 @@ public class CS3 {
                              .reduce((a, b) -> a.add(b).intVar()).get();
     }
 
-    private IntVar costVar(Water target, int targetL, Map<Water, IntVar> wvs) {
-        return square(sum(wvs, w1 -> w1.ca).sub(target.ca * targetL).intVar())
-                 .add(square(sum(wvs, w1 -> w1.mg).sub(target.mg * targetL).intVar()))
-                 .add(square(sum(wvs, w1 -> w1.na).sub(target.na * targetL).intVar()))
-                 .intVar();
+    private int weightedSum(Map<Water, IntVar> wvs, Function<Water, Integer> getter, Map<Salt, IntVar> ss, Function<Salt, Integer> sgetter, int liters) {
+        int wSum = wvs.entrySet().stream()
+                .mapToInt(wv -> wv.getValue().getValue() * getter.apply(wv.getKey()))
+                .sum();
+        int sSum = ss.entrySet().stream()
+                .mapToInt(sv -> sv.getValue().getValue() * sgetter.apply(sv.getKey()))
+                .sum();
+        return (wSum + sSum)/ liters;
     }
 
-    private int weightedSum(Map<Water, IntVar> wvs, Function<Water, Integer> getter, int liters) {
-        return wvs.entrySet().stream()
-                .map(wv -> wv.getValue().getValue() * getter.apply(wv.getKey()))
-                .mapToInt(i -> i)
-                .sum() / liters;
-    }
-
-    private IntVar sum(Map<Water, IntVar> wvs, Function<Water, Integer> getter) {
+    private IntVar waterSum(Map<Water, IntVar> wvs, Function<Water, Integer> getter) {
         return wvs.entrySet().stream()
                 .map(wv -> wv.getValue().mul(getter.apply(wv.getKey())).intVar())
                 .reduce((a, b) -> a.add(b).intVar()).get();
