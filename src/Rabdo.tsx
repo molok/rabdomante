76e26f70e505c9ed7014cd9bdcd4803fcea119e6
Alessio Bolognino <abolognino@pic-informatica.it>
2018-03-20 23:57:15 +0100

    fix bachi

--- src/Rabdo.tsx
+++ src/Rabdo.tsx
@@ -28,34 +28,79 @@ interface RabdoProps {
     saltSelectionChanged: (salts: Array<string>) => void
 }
 
+interface TargetWaterProps {
+    target: WaterDef
+    targetChanged: (w: WaterDef) => void
+}
+
+class TargetWater extends Component<TargetWaterProps, {}> {
+    render() {
+        return (
+            <Panel>
+                <Panel.Heading>
+                    <Panel.Title>Obiettivo</Panel.Title>
+                </Panel.Heading>
+                <Panel.Body>
+                    <FormGroup>
+                        <FormGroup>
+                            <Row>
+                                <MineralInput
+                                    label="litri" symbol=""
+                                    value={this.props.target.l}
+                                    onChange={this.targetChanged.bind(this, "l")} />
+                            </Row>
+                        </FormGroup>
+                        <MineralForm water={this.props.target} attrChanged={this.targetChanged.bind(this)}/>
+                    </FormGroup>
+                </Panel.Body>
+            </Panel>
+            )
+    }
+
+    targetChanged(attrName: string, value: number) {
+        this.props.targetChanged({ ...this.props.target, [attrName]: value });
+    }
+}
+
+interface MineralFormProps {
+    water: WaterDef
+    attrChanged: (attrName: string, value: number) => void
+}
+
+class MineralForm extends Component<MineralFormProps, {}> {
+    render() {
+        return (
+            <>
+            <FormGroup>
+                <Row>
+                    <MineralInput label="Calcio" symbol="Ca" value={this.props.water.ca} onChange={this.props.attrChanged.bind(this.props, "ca")} />
+                    <MineralInput label="Magnesio" symbol="Mg" value={this.props.water.mg} onChange={this.props.attrChanged.bind(this.props, "mg")} />
+                    <MineralInput label="Sodio" symbol="Na" value={this.props.water.na} onChange={this.props.attrChanged.bind(this.props, "na")} />
+                </Row>
+            </FormGroup>
+            <FormGroup>
+            <Row>
+                <MineralInput label="solfati" symbol="SO4" value={this.props.water.so4} onChange={this.props.attrChanged.bind(this.props, "so4")} />
+                <MineralInput label="cloruro" symbol="Cl" value={this.props.water.cl} onChange={this.props.attrChanged.bind(this.props, "cl")} />
+                <MineralInput label="bicarbonati" symbol="HCO3" value={this.props.water.hco3} onChange={this.props.attrChanged.bind(this.props, "hco3")} />
+            </Row>
+            </FormGroup>
+            </>
+        );
+    }
+}
+
 class XRabdo extends Component<RabdoProps, {}> {
     render() {
         return (
             <div className="Rabdo container">
                 <PageHeader className={"text-center"}>
-                    Rabdomante <small>Water Profile Calculator</small>
+                    XRabdomante <small>Water Profile Calculator</small>
                 </PageHeader>
                 <Form horizontal>
                     <FormGroup>
-                        <Panel>
-                            <Panel.Heading>
-                                <Panel.Title>Obiettivo</Panel.Title>
-                            </Panel.Heading>
-                            <Panel.Body>
-                                <FormGroup>
-                                    <FormGroup>
-                                        <Row>
-                                            <MineralInput
-                                                label="litri" symbol=""
-                                                value={this.props.target.l}
-                                                onChange={this.targetChanged.bind(this, "l")} />
-                                            </Row>
-                                    </FormGroup>
-                                    {this.mineralForm(this.props.target, this.targetChanged.bind(this))}
-                                </FormGroup>
-                            </Panel.Body>
-                        </Panel>
-                        {this.renderWaters()}
+                        <TargetWater target={this.props.target} targetChanged={this.props.targetChanged}/>
+                        <SourceWaters sources={this.props.sources} removeSource={this.props.removeSource} sourceChanged={this.props.sourceChanged}/>
                         <Button bsSize="small" onClick={this.addWater.bind(this)}><Glyphicon glyph="plus"/>Aggiungi acqua base</Button>
                     </FormGroup>
 
@@ -76,30 +121,6 @@ class XRabdo extends Component<RabdoProps, {}> {
         this.props.addWater(water());
     }
 
-    togglePanel(idx: number, e: any) {
-        console.log("togglePanel!")
-        e.preventDefault();
-        this.props.sourceChanged(
-            idx,
-            { ...this.props.sources[idx], visible: !this.props.sources[idx].visible }
-        );
-    }
-
-    sourceChanged(idx: number, attrName: string, e: React.ChangeEvent<HTMLInputElement>) {
-        console.log("sourceChanged!")
-        this.props.sourceChanged(idx, { ...this.props.sources[idx], [attrName]: e.target.value });
-    }
-
-    targetChanged(attrName: string, e: React.ChangeEvent<HTMLInputElement>) {
-        this.props.targetChanged({ ...this.props.target, [attrName]: e.target.value });
-    }
-
-    removeSource(idx: number, e: any) {
-        e.preventDefault();
-        e.stopPropagation();
-        this.props.removeSource(idx);
-    }
-
     saltsChanged(e: any) {
         console.log("event:", e);
         let salts = e.map((sel: any) => sel.value);
@@ -115,68 +136,79 @@ class XRabdo extends Component<RabdoProps, {}> {
             <Select multi value={value} options={options} onChange={this.saltsChanged.bind(this)}></Select>
         )
     }
+}
 
-    renderWaters() {
-        let watersPanel: JSX.Element[] = this.props.sources
-                     .map((w, idx) =>
+interface SourceWatersProps {
+    sources: Array<WaterDef>
+    removeSource: (idx: number) => void
+    sourceChanged: (idx: number, w: WaterDef) => void
+}
 
-            <Panel key={idx} eventKey={idx}
-                   expanded={this.props.sources[idx].visible}>
-                <Panel.Heading onClick={this.togglePanel.bind(this, idx)}>
-                    <Panel.Title toggle className="clearfix">{w.name || "Acqua base #" + (idx + 1)}
-                                        <span className={"pull-right"}><Button bsSize="xsmall" onClick={this.removeSource.bind(this, idx)}><Glyphicon glyph="remove"/></Button></span>
-                    </Panel.Title>
-                </Panel.Heading>
-                <Panel.Body collapsible>
-                    <FormGroup>
-                        <Row>
-                            <Col componentClass={ControlLabel} sm={2}>Nome</Col>
-                            <Col sm={4}>
-                                <FormControl name="name" bsSize="small" type="text" placeholder=""
-                                             value={w.name||""}
-                                             onChange={this.sourceChanged.bind(this, idx, "name")}/>
-                            </Col>
-                            <MineralInput
-                                label="litri" symbol=""
-                                value={this.props.sources[idx].l}
-                                onChange={this.sourceChanged.bind(this, idx, "l")} />
-                        </Row>
-                    </FormGroup>
-                    { this.mineralForm(this.props.sources[idx], this.sourceChanged.bind(this, idx)) }
-                </Panel.Body>
-            </Panel>
-        );
+class SourceWaters extends Component<SourceWatersProps, {}> {
+    sourceChanged(idx: number, attrName: string, e: React.ChangeEvent<HTMLInputElement>) {
+        console.log("sourceChanged!");
+        e.stopPropagation();
+        this.props.sourceChanged(idx, { ...this.props.sources[idx], [attrName]: e.target.value });
+    }
+
+    removeSource(idx: number, e: any) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.props.removeSource(idx);
+    }
+
+    render() {
+        // this.props.sources.forEach(w => { console.log("wwwater:", w)});
+        let res =  this.props.sources
+            .map((w: WaterDef, idx: number) => {
+                console.log("xwater:", w, "litri:", w.l, "idx:", idx);
+                    return (
+                        <Panel key={idx} eventKey={idx}
+                               expanded={this.props.sources[idx].visible}>
+                            <Panel.Heading onClick={this.togglePanel.bind(this, idx)}>
+                                <Panel.Title toggle className="clearfix">{w.name || "Acqua base #" + (idx + 1)}
+                                    <span className={"pull-right"}>
+                                <Button bsSize="xsmall" onClick={this.removeSource.bind(this, idx)}><Glyphicon
+                                    glyph="remove"/></Button>
+                            </span>
+                                </Panel.Title>
+                            </Panel.Heading>
+                            <Panel.Body collapsible>
+                                <FormGroup>
+                                    <Row>
+                                        <Col componentClass={ControlLabel} sm={2}>Nome</Col>
+                                        <Col sm={4}>
+                                            <FormControl name="name" bsSize="small" type="text" placeholder=""
+                                                         value={w.name}
+                                                         onChange={this.sourceChanged.bind(this, idx, "name")}/>
+                                        </Col>
+                                        <MineralInput
+                                            label="litri" symbol=""
+                                            value={w.l}
+                                            onChange={this.attrChanged.bind(this, idx, "l")}/>
+                                    </Row>
+                                </FormGroup>
+                                <MineralForm water={w} attrChanged={this.attrChanged.bind(this, idx)}/>
+                            </Panel.Body>
+                        </Panel>
+                    )
+                }
+            );
 
         return (
             <PanelGroup id="source_waters">
-                {watersPanel}
+                {res}
             </PanelGroup>
         );
     }
 
-    private mineralForm(water: WaterDef, onChange: (attrName: String, event: ChangeEvent<HTMLInputElement>) => void) {
-        return (
-        <>
-            <FormGroup>
-                <Row>
-                    <MineralInput label="Calcio" symbol="Ca" value={water.ca} onChange={onChange.bind(this,"ca")} />
-                    <MineralInput label="Magnesio" symbol="Mg" value={water.mg} onChange={onChange.bind(this,"mg")} />
-                    <MineralInput label="Sodio" symbol="Na" value={water.na} onChange={onChange.bind(this,"na")} />
-                </Row>
-            </FormGroup>
-            <FormGroup>
-                <Row>
-                    <MineralInput label="solfati" symbol="SO4" value={water.so4} onChange={onChange.bind( "so4")} />
-                    <MineralInput label="cloruro" symbol="Cl" value={water.cl} onChange={onChange.bind("cl")} />
-                    <MineralInput label="bicarbonati" symbol="HCO3" value={water.hco3} onChange={onChange.bind("hco3")} />
-                </Row>
-            </FormGroup>
-        </>);
+    attrChanged(idx: number, attrName: string, value: any) {
+        this.props.sourceChanged(idx, { ...this.props.sources[idx], [attrName]: value});
     }
 
-    titleCase(str: string) {
-        return str.charAt(0).toUpperCase() +
-            str.substr(1).toLowerCase();
+    togglePanel(idx: number, e: any) {
+        e.preventDefault();
+        this.attrChanged(idx, "visible", !this.props.sources[idx].visible);
     }
 }
 
@@ -184,16 +216,20 @@ interface MineralInputProps {
     label: string,
     symbol: string,
     value: number,
-    onChange: (e: any) => void,
+    onChange: (e: number) => void,
 }
 
 class MineralInput extends Component<MineralInputProps, {}> {
+    changed(e: ChangeEvent<HTMLInputElement>) {
+        this.props.onChange(parseFloat(e.target.value));
+    }
+
     render() {
         let show = this.titleCase(this.props.label) + (this.props.symbol ? " (" + this.props.symbol + ")" : "");
         return (
             <>
                 <Col componentClass={ControlLabel} sm={2}>{show}</Col>
-                <Col sm={1}> <FormControl value={this.props.value} bsSize="small" type="number" placeholder="" onChange={this.props.onChange}/></Col>
+                <Col sm={1}> <FormControl value={this.props.value} bsSize="small" type="number" placeholder="" onChange={this.changed.bind(this)}/></Col>
             </>
         )
     }
