320eb07305ce31b19c4651c33e9e56a23ce62fb6
Alessio Bolognino <abolognino@pic-informatica.it>
2018-06-05 14:36:20 +0200

    allow cors

--- src/main/java/alebolo/rabdomante/ws/SparkRouting.java
+++ src/main/java/alebolo/rabdomante/ws/SparkRouting.java
@@ -4,14 +4,33 @@ package alebolo.rabdomante.ws;
 import alebolo.rabdomante.core.WSolution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.Filter;
 import spark.Request;
 import spark.Response;
+import spark.Spark;
+
+import java.util.HashMap;
 
 import static spark.Spark.*;
 
 
 public class SparkRouting {
     static Logger log = LoggerFactory.getLogger(SparkRouting.class);
+
+    private static final HashMap<String, String> corsHeaders = new HashMap<>();
+
+    static {
+        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        corsHeaders.put("Access-Control-Allow-Origin", "*");
+        corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
+        corsHeaders.put("Access-Control-Allow-Credentials", "true");
+    }
+
+    public final static void allowCors() {
+        Filter filter = (request, response) -> corsHeaders.forEach((key, value) -> response.header(key, value));
+        Spark.after(filter);
+    }
+
 
     public static void start() {
         port(8080);
@@ -24,6 +43,7 @@ public class SparkRouting {
     }
 
     public static void defineResources() {
+        allowCors();
         before((request, response) -> response.type("application/json"));
 
         get("/ping", (req, res) -> {
